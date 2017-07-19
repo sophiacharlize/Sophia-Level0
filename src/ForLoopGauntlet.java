c6e76d0839ff0a0e71a6c3349f821c18211108f7
RhymeLyet <rhymeinparentheses@gmail.com>
2017-07-19 12:01:33 -0700

    Safe cracker

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,21 @@
+
+public class ForLoopGauntlet {
+public static void main(String[] args) {
+//	for (int i = 0; i <= 100; i++) {
+//		System.out.println(i);
+//	}
+//	for (int i = 100; i >= 0; i--) {
+//	System.out.println(i);	
+//	}
+//	for (int i = 2; i < 100; i=i+2) {
+//		System.out.println(i);
+//	}
+//	for (int i = 1; i <= 99; i=i+2) {
+//		System.out.println(i);
+//	}
+	for (int i = 1; i <= 500; i++) {
+		System.out.println(i);
+		
+	}
+}
+}
