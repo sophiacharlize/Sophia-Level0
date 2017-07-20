24750b0c6251a2e8c89b384d286059704ab528f2
RhymeLyet <rhymeinparentheses@gmail.com>
2017-07-20 11:55:52 -0700

    fizz buzz, and for loop gauntlet

--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -1,21 +1,53 @@
 
 public class ForLoopGauntlet {
-public static void main(String[] args) {
-//	for (int i = 0; i <= 100; i++) {
-//		System.out.println(i);
-//	}
-//	for (int i = 100; i >= 0; i--) {
-//	System.out.println(i);	
-//	}
-//	for (int i = 2; i < 100; i=i+2) {
-//		System.out.println(i);
-//	}
-//	for (int i = 1; i <= 99; i=i+2) {
-//		System.out.println(i);
-//	}
-	for (int i = 1; i <= 500; i++) {
-		System.out.println(i);
-		
-	}
-}
-}
+	public static void main(String[] args) {
+		// for (int i = 0; i <= 100; i++) {
+		// System.out.println(i);
+		// }
+		// for (int i = 100; i >= 0; i--) {
+		// System.out.println(i);
+		// }
+		// for (int i = 2; i < 100; i=i+2) {
+		// System.out.println(i);
+		// }
+		// for (int i = 1; i <= 99; i=i+2) {
+		// System.out.println(i);
+		// }
+		// for (int i = 1; i <= 500; i++) {
+		// System.out.print(i);
+		// if(i%2==0) {
+		// System.out.println(" even ");
+		// }
+		// else{
+		// System.out.println(" odd ");
+		// }
+
+		//
+		// }
+		// for (int j = 0; j < 777; j++) {
+		//
+		//
+		// if(j%7==0) {
+		// System.out.println(j);
+		// }
+		// for (int i = 2003; i <= 2017; i++) {
+		// System.out.println(i);
+		// System.out.println(i - 2003);
+		//
+		// }
+
+//		for (int i = 0; i < 3; i++) {
+//			for (int j = 0; j < 3; j++) {
+//				System.out.println(i + " " + j);
+//				
+//			}
+		for (int i = 1; i <= 3; i++) {
+			System.out.print(i);
+		}
+			for (int j = 4; j <= 6; j++) {
+			System.out.println(j);	
+			}
+			
+		}
+		}
+	
